c8717abb5c5adeb9d68a814550e91f1c7ba9eee2
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2026-08-07 10:06:41 +0100

    Added chrome options for CI usage

--- tests/src/test/scala/govuk/utils/TestHelpers.scala
+++ tests/src/test/scala/govuk/utils/TestHelpers.scala
@@ -1,7 +1,7 @@
 package govuk.utils
 
 import io.github.bonigarcia.wdm.WebDriverManager
-import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.{ChromeDriver, ChromeOptions}
 import org.openqa.selenium.support.ui.Select
 import org.openqa.selenium.{By, WebDriver}
 
@@ -15,8 +15,22 @@ object TestHelpers {
 
   def setup(): Unit = {
     WebDriverManager.chromedriver().setup()
-    driver = new ChromeDriver()
-    driver.manage().window().maximize()
+
+    val options = new ChromeOptions()
+
+    if (sys.env.contains("CI")) {
+      options.addArguments(
+        "--headless=new",
+        "--no-sandbox",
+        "--disable-dev-shm-usage"
+      )
+    }
+
+    driver = new ChromeDriver(options)
+
+    if (!sys.env.contains("CI")) {
+      driver.manage().window().maximize()
+    }
   }
 
   def goTo(path: String): Unit = {
